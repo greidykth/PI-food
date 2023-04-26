b0043163f396da1342acf181bfcb3fd1d5d49b75
Greidy Peña <greidykp@gmail.com>
2023-04-25 20:09:27 -0500

    Add diets controllers

---
+++ api/src/controllers/dietsControllers.js
@@ -0,0 +1,35 @@
+const { Diet } = require("../db");
+const { getRecipes } = require("./recipesControllers");
+
+const getDietsFromDb = async () => {
+  const diets = await Diet.findAll();
+  return diets;
+};
+
+const getDietsFromApi = async () => {
+  try {
+    const recipes = await getRecipes();
+    let dietsFromApi = recipes.recipesFromApi
+      .map((recipe) => recipe.diets)
+      .flat();
+    dietsFromApi.push("vegan", "vegetarian", "gluten free")
+    dietsFromApi = [...new Set(dietsFromApi)]; //Elimina registros repetidos del array
+    const diets = dietsFromApi.map((diet) => {
+      return { name: diet };
+    });
+    storeDiets(diets);
+  } catch (error) {
+    console.log("Error during store diets, ", error.message);
+  }
+};
+
+const storeDiets = async (diets) => {
+    await Diet.bulkCreate(diets);
+    console.log("Diets created successfully");
+};
+
+module.exports = {
+  getDietsFromDb,
+  getDietsFromApi,
+  storeDiets,
+};
