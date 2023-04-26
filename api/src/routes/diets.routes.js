bbdb281864b528cf14b0eab93899d4bc9f66de91
Greidy Peña <greidykp@gmail.com>
2023-04-25 20:08:43 -0500

    Add diets routes

--- api/src/routes/diets.routes.js
+++ api/src/routes/diets.routes.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
+const { getDietsFromDb } = require("../controllers/dietsControllers");
+const { STATUS_OK, STATUS_BAD_REQUEST } = require("../utils/const_status");
 
 const dietsRoutes = Router();
 
 dietsRoutes.get("/", async (req, res) => {
-  res.status(200).json("Desde diets");
+  try {
+    const diets = await getDietsFromDb() 
+    res.status(STATUS_OK).json(diets);
+  } catch (error) {
+    res.status(STATUS_BAD_REQUEST).json({ error: error.message });
+  }
 });
+
 
 module.exports = dietsRoutes;
