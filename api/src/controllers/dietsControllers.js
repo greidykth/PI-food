201259d317053212ef2bd77ba8a44a704111260f
Greidy Peña <greidykp@gmail.com>
2023-04-27 10:10:02 -0500

    Fix getDietsFromApi

--- api/src/controllers/dietsControllers.js
+++ api/src/controllers/dietsControllers.js
@@ -1,5 +1,5 @@
 const { Diet } = require("../db");
-const { getRecipes } = require("./recipesControllers");
+const { getRecipesFromApi } = require("./recipesControllers");
 
 const getDietsFromDb = async () => {
   const diets = await Diet.findAll();
@@ -8,15 +8,16 @@ const getDietsFromDb = async () => {
 
 const getDietsFromApi = async () => {
   try {
-    const recipes = await getRecipes();
-    let dietsFromApi = recipes.recipesFromApi
-      .map((recipe) => recipe.diets)
-      .flat();
+    const recipes = await getRecipesFromApi();
+    let dietsFromApi = recipes.map((recipe) => recipe.diets).flat();
+    
     dietsFromApi.push("vegan", "vegetarian", "gluten free")
     dietsFromApi = [...new Set(dietsFromApi)]; //Elimina registros repetidos del array
+    
     const diets = dietsFromApi.map((diet) => {
       return { name: diet };
     });
+
     storeDiets(diets);
   } catch (error) {
     console.log("Error during store diets, ", error.message);
