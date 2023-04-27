8281d61c8b6d0bdad507c9f3c0a834b9d78fbc56
Greidy Peña <greidykp@gmail.com>
2023-04-27 16:30:04 -0500

    Add navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav/Nav";
+import LandingPage from "./views/LandingPage/LandingPage";
+import NotFound from "./views/NotFound/NotFound";
+import Home from "./views/Home/Home";
+import Detail from "./views/Detail/Detail";
+import Form from "./views/Form/Form";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      {location.pathname === "/home" && <Nav />}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/recipes/:id" element={<Detail />} />
+        <Route path="/recipes/create" element={<Form />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
