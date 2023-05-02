894a4434938e76f17aa3776d9a4f93645a1f0f9b
Greidy Peña <greidykp@gmail.com>
2023-05-02 15:30:15 -0500

    Add index views

---
+++ client/src/views/index.js
@@ -0,0 +1,7 @@
+import Detail from "./Detail/Detail"
+import Form from "./Form/Form"
+import Home from "./Home/Home"
+import LandingPage from "./LandingPage/LandingPage"
+import NotFound from "./NotFound/NotFound"
+
+export { Detail, Form, Home, LandingPage, NotFound };
