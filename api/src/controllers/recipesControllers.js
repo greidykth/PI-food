317722d503777e301fc1c240ee96a84b6bae4b68
Greidy Peña <greidykp@gmail.com>
2023-05-02 15:27:34 -0500

    Add order in recipes

--- api/src/controllers/recipesControllers.js
+++ api/src/controllers/recipesControllers.js
@@ -63,6 +63,7 @@ const getRecipesByName = async (name) => {
       },
     },
     attributes: ["id", "name", "image"],
+    order: [["createdAt", "DESC"]],
   });
 
   const recipesDbWithDiets = await Promise.all(
