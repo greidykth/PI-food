802679ff22577b25dc13d2d6fe556b0b896afd7b
Greidy Peña <greidykp@gmail.com>
2023-05-02 21:45:24 -0500

    Fix Recipe Model

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,53 +1,36 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    summary: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    healthScore: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    procedure: {
-      type: DataTypes.ARRAY(DataTypes.JSON),
-    allowNull: false,
-    }
-  },
-  {
-    getterMethods: {
-      async allDiets() {
-        const recipe = this.toJSON();
-        const diets = await this.getDiets();
-        recipe.diets = diets.map((diet) => diet.name);
-        return recipe;
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
       },
-    }}
-  )
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      healthScore: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      procedure: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        allowNull: false,
+      },
+    },
+  );
 };
-
-
-
-
-
-
-
-
-
-
