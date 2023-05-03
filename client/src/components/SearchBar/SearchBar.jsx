1ea581c5b2f249e0927bad5d381c8543d71d2752
Greidy Peña <greidykp@gmail.com>
2023-05-02 21:52:01 -0500

    Add search by name

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getRecipesByName } from "../../redux/actions/actions";
 
 const SearchBar = () => {
+  const dispatch = useDispatch();
+  const [inputSearch, setInputSearch] = useState("");
+
+  const handleChange = (event) => {
+    setInputSearch(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(getRecipesByName(inputSearch));
+  };
+
+  const handleKeyUp = (event) => {
+    if ( event.keyCode === 13 || event.charCode === 13 ) {
+      dispatch(getRecipesByName(inputSearch));
+    }
+  };
+
   return (
     <div>
-      <input type="text" placeholder="Find a recipe" />
-      <button>Search</button>
+      <input
+        className=""
+        value={inputSearch}
+        type="text"
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
+        placeholder="Find a recipe"
+      />
+      <button onClick={handleSubmit}>Search</button>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
