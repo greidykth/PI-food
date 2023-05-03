f6fac8b1829173253d038ecf8a1266a155562b09
Greidy Peña <greidykp@gmail.com>
2023-05-02 21:49:18 -0500

    Fix nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
+import style from "./nav.module.css";
 
 const Nav = () => {
   return (
-    <div>
-      <button>Filters</button>
-      <NavLink to="/recipes/create">
-        <button>Create Recipe</button>
-      </NavLink>
+    <div className={style.mainContainer}>
+      <Link to="/home">Home</Link>
+      <Link to="/recipes/create">Create Recipe</Link>
+      <Link to="/home">Filters</Link>
       <SearchBar />
     </div>
   );
