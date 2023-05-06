f0a9595bea874fe6aa9b3383949b27f30febd515
Greidy Peña <greidykp@gmail.com>
2023-05-06 12:03:12 -0500

    Fix recipes

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -4,34 +4,28 @@ import Recipe from "../Recipe/Recipe";
 import style from "./recipes.module.css";
 
 const Recipes = () => {
-  const { allRecipes } = useSelector((state) => state);
+  const { filteredRecipes } = useSelector((state) => state);
+
 
   return (
     <div className={style.container}>
-      {allRecipes.recipesFromDb && (
+      {filteredRecipes && (
         <>
-          {allRecipes.recipesFromDb?.map((recipe) => (
+          {filteredRecipes.map((recipe) => (
             <Recipe
               key={recipe.id}
-              id={recipe.id}
+              id={ recipe.createdOnDB ? recipe.id : "A" + recipe.id }
               name={recipe.name}
               image={recipe.image}
               diets={recipe.diets}
+              healthScore={recipe.healthScore}
+              source={ recipe.createdOnDB ? "DB" : "API" }
             />
           ))}
-          {allRecipes.recipesFromApi?.map((recipe) => (
-            <Recipe
-              key={recipe.id}
-              id={`A${recipe.id}`}
-              name={recipe.name}
-              image={recipe.image}
-              diets={recipe.diets}
-            />
-          ))} 
         </>
       )}
-      {!allRecipes.recipesFromDb && "Loading..."}
-      {allRecipes.recipesFromDb?.length === 0 && allRecipes.recipesFromApi?.length === 0 && "There are not recipes to show"}
+      {filteredRecipes.length === 0 && "Loading..."}
+      {/* {filteredRecipes.length === 0 && "There are not recipes to show"} */}
     </div>
   );
 };
