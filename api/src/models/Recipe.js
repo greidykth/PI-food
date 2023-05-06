43aaac6477a71e54f484235d5f1edc83f29eecf1
Greidy Peña <greidykp@gmail.com>
2023-05-06 12:02:24 -0500

    Add createdOnDB in model

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,38 +3,40 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define(
-    "recipe",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      image: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      summary: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      healthScore: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          min: 0,
-          max: 100
-        }
-      },
-      procedure: {
-        type: DataTypes.ARRAY(DataTypes.JSON),
-        allowNull: false,
+  sequelize.define("recipe", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 0,
+        max: 100,
       },
     },
-  );
+    procedure: {
+      type: DataTypes.ARRAY(DataTypes.JSON),
+      allowNull: false,
+    },
+    createdOnDB: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  });
 };
