8e6c2e0219fab63716bbf6e2ebdec40eaa6378db
Greidy Peña <greidykp@gmail.com>
2023-05-06 12:00:55 -0500

    Add filters in nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import style from "./nav.module.css";
+import Filters from "../Filters/Filters";
 
 const Nav = () => {
+  const [showFilters, setShowFilters] = useState(false);
+
+  const showFiltersHandler = () => {
+    setShowFilters(!showFilters);
+  };
+
   return (
-    <div className={style.mainContainer}>
-      <Link to="/home">Home</Link>
-      <Link to="/recipes/create">Create Recipe</Link>
-      <Link to="/home">Filters</Link>
-      <SearchBar />
-    </div>
+      <div className={style.mainContainer}>
+        <div className={style.navigation}>
+        <Link to="/home">Home</Link>
+        <Link to="/recipes/create">Create Recipe</Link>
+        <Link onClick={showFiltersHandler}>Filters</Link>
+        <SearchBar />
+        </div>
+        <div className="">
+        {showFilters && <Filters />}
+        </div>
+      </div>
   );
 };
 
