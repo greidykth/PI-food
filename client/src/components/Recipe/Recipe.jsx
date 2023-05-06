4d7f6741d42ca48da0459c1c79235e84b61c409e
Greidy Peña <greidykp@gmail.com>
2023-05-06 12:04:10 -0500

    Add health score in recipe

--- client/src/components/Recipe/Recipe.jsx
+++ client/src/components/Recipe/Recipe.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import style from "./recipe.module.css";
 
-const Recipe = ({id, name, image, diets }) => {
+const Recipe = ({id, name, image, diets, source, healthScore }) => {
 
   return (
     <div className={style.recipe} >
       <div className="">
+      <h5 className="">{source}</h5>
+      <h5 className="">Health score: {healthScore}</h5>
         <Link className="" to={`/recipes/${id}`}>
           <h3 className="">{name}</h3>
         </Link>
