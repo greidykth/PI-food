754d43ca9e9c6e005c8a745c9661c813fcf4053e
Greidy Peña <greidykp@gmail.com>
2023-05-07 11:19:11 -0500

    Add number of results

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -6,26 +6,27 @@ import style from "./recipes.module.css";
 const Recipes = () => {
   const { filteredRecipes } = useSelector((state) => state);
 
-
   return (
     <div className={style.container}>
-      {filteredRecipes && (
-        <>
-          {filteredRecipes.map((recipe) => (
-            <Recipe
-              key={recipe.id}
-              id={ recipe.createdOnDB ? recipe.id : "A" + recipe.id }
-              name={recipe.name}
-              image={recipe.image}
-              diets={recipe.diets}
-              healthScore={recipe.healthScore}
-              source={ recipe.createdOnDB ? "DB" : "API" }
-            />
-          ))}
-        </>
-      )}
-      {filteredRecipes.length === 0 && "Loading..."}
-      {/* {filteredRecipes.length === 0 && "There are not recipes to show"} */}
+      <h3>Recipes found: {filteredRecipes.length}</h3>
+      <div className={style.cardsContainer}>
+        {filteredRecipes && (
+          <>
+            {filteredRecipes.map((recipe) => (
+              <Recipe
+                key={recipe.id}
+                id={recipe.createdOnDB ? recipe.id : "A" + recipe.id}
+                name={recipe.name}
+                image={recipe.image}
+                diets={recipe.diets}
+                healthScore={recipe.healthScore}
+                source={recipe.createdOnDB ? "DB" : "API"}
+              />
+            ))}
+          </>
+        )}
+        {filteredRecipes.length === 0 && "Loading..."}
+      </div>
     </div>
   );
 };
