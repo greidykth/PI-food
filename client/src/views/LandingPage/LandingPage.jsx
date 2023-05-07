1f4ca5920a74965c6ce02933195446b6025717d8
Greidy Peña <greidykp@gmail.com>
2023-05-07 11:21:56 -0500

    Add landing css

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import style from "./landingPage.module.css";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Flavor Fusion</h1>
-      <h2>
-        Welcome to our delicious world of recipes! This app is here to provide
-        you with endless inspiration and ideas for your next meal. Explore our
-        collection of recipes from around the world.
-      </h2>
-      <NavLink to="/home">
-        <button>Let's get stared!</button>
-      </NavLink>
+    <div className={style.principalContainer}>
+      <div className={style.textContainer}>
+        <h1>Flavor Fusion</h1>
+        <p>
+          Welcome to our delicious world of recipes! This app is here to provide
+          you with endless inspiration and ideas for your next meal. Explore our
+          collection of recipes from around the world.
+        </p>
+        <NavLink to="/home">
+          <button className={style.buttonHome}>Let's get stared!</button>
+        </NavLink>
+      </div>
     </div>
   );
 };
