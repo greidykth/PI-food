d209e40c127e68c7804fabf6ee98f1e8563d3655
Greidy Peña <greidykp@gmail.com>
2023-05-08 11:31:06 -0500

    Add port in index

--- api/index.js
+++ api/index.js
@@ -19,16 +19,18 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
 const { getDietsFromApi } = require('./src/controllers/dietsControllers.js');
 const { Diet } = require('./src/db.js');
 
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(async () => {
   const diets = await Diet.findAll(); 
   if (diets.length === 0) { // verifica si la tabla diet esta vacia, la llena
     await getDietsFromApi();
   }
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
