ec11756bb3ea061f4b17f4938acc20c4b1e16d4d
Greidy Peña <greidykp@gmail.com>
2023-05-09 11:12:29 -0500

    Add text not found

--- client/src/views/NotFound/NotFound.jsx
+++ client/src/views/NotFound/NotFound.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import style from "./notfound.module.css";
 
 const NotFound = () => {
   return (
-    <div>
-      Not found
+    <div className={style.principalContainer}>
+      <div className={style.textContainer}>
+        <h1>ERROR</h1>
+        <h1>404</h1>
+        <h3>This page is outside of the universe</h3>
+        <p>
+          The page you are trying to access does not exist or has been moved.
+          Try going back to our homepage.
+        </p>
+        <NavLink to="/home">
+          <button className={style.buttonHome}>Go Home!</button>
+        </NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
