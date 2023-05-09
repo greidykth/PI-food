916ff6a113ea73281533b5c42df0725541a9ca3d
Greidy Peña <greidykp@gmail.com>
2023-05-08 20:18:20 -0500

    Add nav in form

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import { Home, Form, LandingPage, Detail, NotFound } from "./views";
+import { useSelector } from "react-redux";
+import Notification from "./components/Notification/Notification";
 
 function App() {
   const location = useLocation();
+  const { notification } = useSelector((state) => state);
 
   return (
     <div className="App">
-      {(location.pathname === "/home" || location.pathname === "/recipes/create")  && <Nav />}
+      {(location.pathname === "/home" ||
+        location.pathname === "/recipes/create") && <Nav />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
@@ -16,6 +20,7 @@ function App() {
         <Route path="/recipes/create" element={<Form />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      {notification.message !== "" && <Notification />}
     </div>
   );
 }
