79a2236f2cf547dbb38c3dd266f2d4b106a53759
Greidy Peña <greidykp@gmail.com>
2023-05-09 15:37:18 -0500

    Fix helper

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,22 +1,3 @@
-export const filterByDietHelper = (arr, parameter) => {
-    
-  let recipesTemporal = [];
-  let recipesTemporal1 = [];
-
-      parameter.forEach(diet => {
-        if(diet.checked)
-          recipesTemporal = [...recipesTemporal, arr.filter(recipe => recipe.diets.includes(diet.name))]
-      });
-      recipesTemporal.forEach(recipe => {
-        if (!recipesTemporal1.find((recipeTemporal) => recipeTemporal.id === recipe.id)) {
-          recipesTemporal1.push(recipe);
-        }
-      })
-      
-      return [...recipesTemporal1];
-
-}
-
 export const orderByNameHelper = (arr, parameter) => {
     const newOrder = arr.sort((a, b) => {
         if (a.name > b.name) {
