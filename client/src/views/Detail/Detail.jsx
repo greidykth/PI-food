9240c0a24955c68e2e75ce593d4bdb6c3ed8ac10
Greidy Peña <greidykp@gmail.com>
2023-05-08 20:25:59 -0500

    Add loader indetail

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useParams } from 'react-router-dom'
-import { cleanDetailRecipe, getDetailRecipe } from '../../redux/actions/actions';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useParams } from "react-router-dom";
+import {
+  cleanDetailRecipe,
+  getDetailRecipe,
+} from "../../redux/actions/actions";
+import Loader from "../../components/Loader/Loader";
 
 const Detail = () => {
   const { id } = useParams();
@@ -12,40 +16,41 @@ const Detail = () => {
     dispatch(getDetailRecipe(id));
     return () => {
       dispatch(cleanDetailRecipe());
-    }
+    };
   }, [id, dispatch]);
 
- 
   return (
-    <div>Desde Detail
+    <div>
+      Desde Detail
       <NavLink to="/home">
         <button>Go back</button>
       </NavLink>
-      {
-        recipe.name ? (<div>
+      {recipe.name ? (
+        <div>
           <h2>{id}</h2>
           <h2>{recipe.name}</h2>
           <img src={recipe.image} alt={`img_${recipe.image}`} />
           <h2>Diets</h2>
           <ul className="">
-              {recipe.diets?.map((diet) => (
-                <li key={diet}>{diet}</li>
-              ))}
+            {recipe.diets?.map((diet) => (
+              <li key={diet}>{diet}</li>
+            ))}
           </ul>
           <p>Summary:</p>
           <p dangerouslySetInnerHTML={{ __html: recipe.summary }}></p>
           <h2>Health Score: {recipe.healthScore}</h2>
           <h2>Method</h2>
           <ol className="">
-              {recipe.procedure?.map((step) => (
-                <li key={step.number}>{step.step}</li>
-              ))}
+            {recipe.procedure?.map((step) => (
+              <li key={step.number}>{step.step}</li>
+            ))}
           </ol>
-
-        </div>) : (<p>Receta cargando ...</p>)
-      }
+        </div>
+      ) : (
+        <Loader />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
