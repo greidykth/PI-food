9d32c891c489df92d43674e150a387a2b32e39b7
Greidy Peña <greidykp@gmail.com>
2023-05-09 11:13:52 -0500

    Focus up detail

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -6,11 +6,17 @@ import {
   getDetailRecipe,
 } from "../../redux/actions/actions";
 import Loader from "../../components/Loader/Loader";
+import style from "./detail.module.css";
+import { ReactComponent as Back } from "../../icons/tarjeta-de-direccion.svg";
 
 const Detail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const recipe = useSelector((state) => state.recipeDetailed);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   useEffect(() => {
     dispatch(getDetailRecipe(id));
@@ -20,35 +26,56 @@ const Detail = () => {
   }, [id, dispatch]);
 
   return (
-    <div>
-      Desde Detail
-      <NavLink to="/home">
-        <button>Go back</button>
-      </NavLink>
-      {recipe.name ? (
-        <div>
-          <h2>{id}</h2>
-          <h2>{recipe.name}</h2>
-          <img src={recipe.image} alt={`img_${recipe.image}`} />
-          <h2>Diets</h2>
-          <ul className="">
-            {recipe.diets?.map((diet) => (
-              <li key={diet}>{diet}</li>
-            ))}
-          </ul>
-          <p>Summary:</p>
-          <p dangerouslySetInnerHTML={{ __html: recipe.summary }}></p>
-          <h2>Health Score: {recipe.healthScore}</h2>
-          <h2>Method</h2>
-          <ol className="">
-            {recipe.procedure?.map((step) => (
-              <li key={step.number}>{step.step}</li>
-            ))}
-          </ol>
+    <div className={style.principalContainer}>
+      <div className={style.container}>
+        <div className={style.backButtonDiv}>
+          <NavLink to="/home">
+            {" "}
+            <Back /> Back to home
+          </NavLink>
         </div>
-      ) : (
-        <Loader />
-      )}
+        <div className={style.recipeContainer}>
+          {recipe.name ? (
+            <div className={style.detailDiv}>
+              <img src={recipe.image} alt={`img_${recipe.image}`} />
+              <div className={style.infoDetail}>
+                <h3 className={style.healthScore}>
+                  Health Score: {recipe.healthScore}
+                </h3>
+                <h4 className={style.id}>{recipe.createdOnDB ? "My recipe" : "Other recipe"}</h4>
+                <h3 className={style.id}>ID {id}</h3>
+                <h2 className={style.name}>{recipe.name}</h2>
+                <h3 className={style.diets}>Diets:</h3>
+                <div className={style.dietsSelectedContainer}>
+                  {recipe.diets?.map((diet) => (
+                    <span key={diet} className={style.spanDiet}>
+                      {diet}
+                    </span>
+                  ))}
+                </div>
+                <h3 className={style.summary}>Summary:</h3>
+                <p dangerouslySetInnerHTML={{ __html: recipe.summary }}></p>
+                <h3 className={style.method}>Method:</h3>
+                <div className={style.stepsDiv}>
+                  {recipe.procedure?.map((step) => (
+                    <div className={style.step}>
+                      <h4 className={style.stepNumber} key={step.number}>Step: {step.number}</h4>
+                      <p className={style.stepParagraph}key={step.number}>{step.step}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ) : (
+            <>
+              <h2>Preparing recipe...</h2>
+              <div className={style.loader}>
+                <Loader />
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
