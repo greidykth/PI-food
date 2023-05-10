0feed6ca1b8c5afdd2e18e599ee8e2231252295a
Greidy Peña <greidykp@gmail.com>
2023-05-10 09:58:49 -0500

    Add social media in landing

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import style from "./landingPage.module.css";
+import { ReactComponent as Github } from "../../icons/icons8-github.svg";
+import { ReactComponent as Linkedin } from "../../icons/icons8-linkedin.svg";
 
 const LandingPage = () => {
   return (
@@ -12,7 +14,23 @@ const LandingPage = () => {
           you with endless inspiration and ideas for your next meal. Explore our
           collection of recipes from around the world.
         </p>
-        <p>Individual project made by Greidy Peña.</p>
+        <p>Individual project made by <b>Greidy Peña.</b></p>
+        <div>
+          <Link
+            className={style.brandIcon}
+            to="https://www.linkedin.com/in/greidypena/"
+            target="_blank"
+          >
+            <Linkedin />
+          </Link>
+          <Link
+            className={style.brandIcon}
+            to="https://github.com/greidykth"
+            target="_blank"
+          >
+            <Github />
+          </Link>
+        </div>
         <NavLink to="/home">
           <button className={style.buttonHome}>Let's get stared!</button>
         </NavLink>
